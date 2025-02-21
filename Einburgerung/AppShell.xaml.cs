de77f0d05349a269137417a62806ff9265b9530a
fodare <folorunsodamilare007@hotmail.com>
2025-02-21 12:34:54 +0100

    Initial commit.

---
+++ Einburgerung/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Einburgerung
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Einburgerung/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Einburgerung;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
