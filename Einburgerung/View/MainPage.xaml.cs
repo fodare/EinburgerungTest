eeb475c6f50681bee504c888f2bade5bd75ca7c9
fodare <folorunsodamilare007@hotmail.com>
2025-02-21 14:49:17 +0100

    Add bining context and testing services integration.

--- Einburgerung/View/MainPage.xaml.cs
+++ Einburgerung/View/MainPage.xaml.cs
@@ -1,24 +1,13 @@
-﻿namespace Einburgerung.View
+﻿using Einburgerung.ViewModel;
+
+namespace Einburgerung.View
 {
 	public partial class MainPage : ContentPage
 	{
-		int count = 0;
-
-		public MainPage()
+		public MainPage(MainPageViewModel mainPageViewModel)
 		{
 			InitializeComponent();
-		}
-
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-
-			if (count == 1)
-				CounterBtn.Text = $"Clicked {count} time";
-			else
-				CounterBtn.Text = $"Clicked {count} times";
-
-			SemanticScreenReader.Announce(CounterBtn.Text);
+			BindingContext = mainPageViewModel;
 		}
 	}
 
