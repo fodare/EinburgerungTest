9fbddcf445e307835496856e549d5c663b721f06
fodare <folorunsodamilare007@hotmail.com>
2025-03-02 12:51:25 +0100

    Initial state_question_model

---
+++ Einburgerung/Model/StateQuestionModel.cs
@@ -0,0 +1,17 @@
+namespace Einburgerung.Model
+{
+    public class StateQuestionModel
+    {
+        public string? Num { get; set; }
+        public string? Question { get; set; }
+        public string? A { get; set; }
+        public string? B { get; set; }
+        public string? C { get; set; }
+        public string? D { get; set; }
+        public string? Solution { get; set; }
+        public string? Image { get; set; }
+        public Translation? Translation { get; set; }
+        public string? Category { get; set; }
+        public string? Context { get; set; }
+    }
+}
