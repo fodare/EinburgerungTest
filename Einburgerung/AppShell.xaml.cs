cdd15ca30155acc44b4ed0e58933ff88adc8e6da
fodare <folorunsodamilare007@hotmail.com>
2025-03-02 11:38:15 +0100

    Add state questions page routing.

--- Einburgerung/AppShell.xaml.cs
+++ Einburgerung/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace Einburgerung
+﻿using Einburgerung.View;
+
+namespace Einburgerung
 {
 	public partial class AppShell : Shell
 	{
 		public AppShell()
 		{
 			InitializeComponent();
+			Routing.RegisterRoute(nameof(StateQuestionsPage), typeof(StateQuestionsPage));
 		}
 	}
 }
