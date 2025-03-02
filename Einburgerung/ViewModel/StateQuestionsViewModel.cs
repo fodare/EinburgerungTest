cc741fca2dc906ca39184d0b50c26dcc30cec6c3
fodare <folorunsodamilare007@hotmail.com>
2025-03-02 11:39:15 +0100

    Initial state questions page.

---
+++ Einburgerung/ViewModel/StateQuestionsViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Einburgerung.ViewModel
+{
+    public partial class StateQuestionsViewModel : BaseViewModel
+    {
+        public StateQuestionsViewModel()
+        {
+            Title = "State Questions";
+        }
+    }
+}
