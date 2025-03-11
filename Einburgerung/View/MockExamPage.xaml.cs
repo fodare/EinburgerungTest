659f1ecce01ceea0bd1b7356a717980df133b0aa
fodare <folorunsodamilare007@hotmail.com>
2025-03-11 15:29:01 +0100

    Dotnet format.

--- Einburgerung/View/MockExamPage.xaml.cs
+++ Einburgerung/View/MockExamPage.xaml.cs
@@ -2,12 +2,12 @@ using Einburgerung.ViewModel;
 
 namespace Einburgerung.View
 {
-	public partial class MockExamPage : ContentPage
-	{
-		public MockExamPage(MockExamPageViewModel mockExamPageViewModel)
-		{
-			InitializeComponent();
-			BindingContext = mockExamPageViewModel;
-		}
-	}
+    public partial class MockExamPage : ContentPage
+    {
+        public MockExamPage(MockExamPageViewModel mockExamPageViewModel)
+        {
+            InitializeComponent();
+            BindingContext = mockExamPageViewModel;
+        }
+    }
 }
