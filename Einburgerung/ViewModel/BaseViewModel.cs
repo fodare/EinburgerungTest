9c00f93224d6850618623670e43171312a106990
fodare <folorunsodamilare007@hotmail.com>
2025-03-17 22:34:28 +0100

    Add basic constructor

--- Einburgerung/ViewModel/BaseViewModel.cs
+++ Einburgerung/ViewModel/BaseViewModel.cs
@@ -4,12 +4,17 @@ namespace Einburgerung.ViewModel
 {
     public partial class BaseViewModel : ObservableObject
     {
+        public BaseViewModel()
+        {
+
+        }
+
         [ObservableProperty]
         private string title = "";
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotBusy))]
-        private bool isBusy = false;
+        private bool isBusy;
 
         public bool IsNotBusy => !IsBusy;
     }
